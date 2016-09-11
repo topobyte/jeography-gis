4cb61196f1a876054a1102ba06b7360375ec96ac
Sebastian Kürten <sebastian@topobyte.de>
2016-09-12 00:25:11 +0200

    Added TestUtil

---
+++ src/test/java/de/topobyte/jeography/viewer/TestUtil.java
@@ -0,0 +1,42 @@
+// Copyright 2016 Sebastian Kuerten
+//
+// This file is part of jeography.
+//
+// jeography is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jeography is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with jeography. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.jeography.viewer;
+
+import java.awt.Window;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @author Sebastian Kuerten (sebastian@topobyte.de)
+ */
+public class TestUtil
+{
+
+	public static void exitOnClose(Window window)
+	{
+		window.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+
+		});
+	}
+
+}
