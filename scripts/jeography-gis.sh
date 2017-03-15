4be2b2c9ed361b15a6ae13e3e264464d6e66a6c4
Sebastian Kürten <sebastian@topobyte.de>
2017-03-15 12:01:57 +0100

    Split into core and exe subprojects

--- scripts/jeography-gis.sh
+++ scripts/jeography-gis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-LIBS="$DIR/../build/lib-run"
-LOG_CONFIG="$DIR/../log4j.properties"
+LIBS="$DIR/../exe/build/lib-run"
+LOG_CONFIG="$DIR/../exe/log4j.properties"
 
 if [ ! -d "$LIBS" ]; then
 	echo "Please run 'gradle createRuntime'"
