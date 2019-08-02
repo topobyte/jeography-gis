7718461bf21a632ffa02621516ad47dcd7f40005
Sebastian Kürten <sebastian@topobyte.de>
2019-08-02 12:25:32 +0200

    Install to ~/topobyte/jeography-gis/jeography-gis-snapshot

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 ./gradlew clean installDist postInstallScript
 
-mkdir -p ~/share/topobyte
-rsync -av --delete exe/build/install/jeography-gis ~/share/topobyte/
+TARGET="$HOME/share/topobyte/jeography-gis/jeography-gis-snapshot"
+
+mkdir -p "$TARGET"
+rsync -av --delete exe/build/install/jeography-gis/ "$TARGET"
 
 ./exe/build/setup/post-install.sh
