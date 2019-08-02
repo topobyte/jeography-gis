4c76f5c3a9e1e11ca852b15b620438ab04c065c8
Sebastian Kürten <sebastian@topobyte.de>
2019-08-02 12:34:48 +0200

    Make install.sh work from anywhere

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+DIR=$(dirname $0)
+
+pushd "$DIR" > /dev/null
 ./gradlew clean installDist postInstallScript
+popd
 
 TARGET="$HOME/share/topobyte/jeography-gis/jeography-gis-snapshot"
 
 mkdir -p "$TARGET"
-rsync -av --delete exe/build/install/jeography-gis/ "$TARGET"
+rsync -av --delete "$DIR/exe/build/install/jeography-gis/" "$TARGET"
 
-./exe/build/setup/post-install.sh
+"$DIR"/exe/build/setup/post-install.sh
