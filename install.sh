3ebc26a1145dc43fdf7bebe93953e781c9517d40
Sebastian Kürten <sebastian@topobyte.de>
2019-08-04 12:32:33 +0200

    Update launch-and-install-plugin

--- install.sh
+++ install.sh
@@ -5,12 +5,8 @@ set -e
 DIR=$(dirname $0)
 
 pushd "$DIR" > /dev/null
-./gradlew clean installDist postInstallScript
+./gradlew clean installDist setupScripts
 popd
 
-TARGET="$HOME/share/topobyte/jeography-gis/jeography-gis-snapshot"
-
-mkdir -p "$TARGET"
-rsync -av --delete "$DIR/exe/build/install/jeography-gis/" "$TARGET"
-
+"$DIR"/exe/build/setup/install.sh
 "$DIR"/exe/build/setup/post-install.sh
